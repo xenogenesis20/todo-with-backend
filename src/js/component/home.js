92c7cbeb18db95639d0e708d0fc5828e7f6d3da6
xenogenesis20 <xenogenesis20@outlook.com>
2021-05-20 00:32:53 +0000

    lots of progress

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	const [inputValue, setInputValue] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	const addTodo = input => {
+		if (input) {
+			setTodoList([...todoList, input]);
+		} else {
+			alert("Please add a task");
+		}
+	};
+	const deleteTodo = indexToRemove => {
+		let alteredList = todoList.filter((value, i) => i != indexToRemove);
+		setTodoList(alteredList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>Maikel&apos;s Todo List</h1>
+			<div className="input-group mb-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Add a task todo."
+					aria-label="Recipient's username"
+					aria-describedby="button-addon2"
+					onChange={e => setInputValue(e.target.value)}
+					value={inputValue}
+				/>
+				<div className="input-group-append">
+					<button
+						className="btn btn-secondary"
+						type="button"
+						id="button-addon2"
+						onClick={() => {
+							addTodo(inputValue);
+							setInputValue("");
+						}}>
+						Add Todo
+					</button>
+				</div>
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-12">
+					{todoList.map((value, i) => {
+						return (
+							<React.Fragment key={i}>
+								<div className="d-flex justify-content-between">
+									{value}{" "}
+									<i
+										onClick={() => deleteTodo(i)}
+										className="fas fa-trash ml-auto"></i>{" "}
+								</div>
+							</React.Fragment>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 }
